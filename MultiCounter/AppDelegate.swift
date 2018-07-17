93b6936feeb9b0d792f7644829e48af6f005399f
Aaron Kollasch <awkollasch@gmail.com>
2018-07-16 21:22:35 -0400

    Initial Commit

---
+++ MultiCounter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MultiCounter
+//
+//  Created by Aaron on 7/16/18.
+//  Copyright © 2018 Aaron Kollasch. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MultiCounter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MultiCounter
+//
+//  Created by Aaron on 7/16/18.
+//  Copyright © 2018 Aaron Kollasch. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
